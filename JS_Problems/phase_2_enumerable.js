1b8b02dcb880e34753d0f18d81639fc1d38c20d2
Dennis <dmyasnyankin@berkeley.edu>
2018-11-22 15:53:10 -0800

    Work on enumerables in JS

--- JS_Problems/phase_2_enumerable.js
+++ JS_Problems/phase_2_enumerable.js
@@ -1,5 +1,25 @@
+const NUMS = [1,2,3,4,5];
+
 //myEach
 
-//myMap
+Array.prototype.myEach = function (func) {
+  for (let i = 0; i < this.length; i++) {
+    func(this[i]);
+  }
+};
 
+NUMS.myEach((num) => {
+  console.log(`square of ${num} is ${num * num}`);
+});
+
+// Array#.myMap
+Array.prototype.myMap = function (func) {
+  const mappedArray = [];
+
+  this.myEach(element => mappedArray.push(func(element)) );
+
+  return mappedArray;
+};
+
+console.log(NUMS.myMap( num => num * num ));
 //myReduce
